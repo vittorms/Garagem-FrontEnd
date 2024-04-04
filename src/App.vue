618345f7c9fab10fd0f491f15fd2b4cf6cd122a3
vittorms <vitor.msalomao@gmail.com>
2024-04-04 09:57:07 -0300

    Adição das rotas no App

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 import router from './router';
 <template>
   <header>
-    <router-link :to="{ name: 'home' }">Home</router-link> |
+    <router-link :to="{ name: 'home' }">Home</router-link> 
     <router-link to="/categorias">Categorias</router-link> 
+    <router-link to="/cores">Cores</router-link> 
+    <router-link to="/acessorios">Acessorios</router-link> 
+    <router-link to="/marcas">Marcas</router-link> 
   </header>
   <main>
     <RouterView />
