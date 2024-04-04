c1c7eb313d9d958571844e3c74e2dab923969158
vittorms <vitor.msalomao@gmail.com>
2024-04-04 09:56:44 -0300

    Adição dos routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import CategoriaView from "../views/CategoriaView.vue";
+import CorView from "../views/CorView.vue";
+import MarcaView from "../views/MarcaView.vue";
+import AcessorioView from "../views/AcessorioView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +18,21 @@ const router = createRouter({
       name: "categorias",
       component: CategoriaView,
     },
+    {
+      path: "/acessorios",
+      name: "acessorios",
+      component: AcessorioView,
+    },
+    {
+      path: "/marcas",
+      name: "marcas",
+      component: MarcaView,
+    },
+    {
+      path: "/cores",
+      name: "cores",
+      component: CorView,
+    }
   ],
 });
 
